a9dcdc68a5d567bd805050ea32b4cdc2f015a296
dan-schel <me@danschellekens.com>
2024-04-09 18:06:36 +1000

    initial commit

---
+++ src/client/java/com/danschellekens/mc/DansUtilsClient.java
@@ -0,0 +1,10 @@
+package com.danschellekens.mc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DansUtilsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
